c31042c4f303936e9b74806f5781d3cd3e9393cf
yl <d3ass13@ya.ru>
2023-03-10 16:29:42 +0300

    cmt1

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from data import db_session
+from data.users import User
+
+
+db_session.global_init("db/blogs.db")
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    add_u
+    #app.run()
+
+def add_user():
+    user = User()
+    user.name = "Пользователь 1"
+    user.about = "биография пользователя 1"
+    user.email = "email@email.ru"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+
+
+if __name__ == '__main__':
+    main()
+
+
